9023cd173b07692a875f9d500447e9d0f7abe7f3
Maksymilian Kuś <max@MacBook-Air-Maksymilian.local>
2023-02-15 21:13:23 +0100

    Add krynica.cpp

--- krynica.cpp
+++ krynica.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 int main() {
    ifstream ifile;
-   ifile.open("input.txt");
+   ifile.open("input.txt", ios::in);
    if(ifile) {
       cout<<"file exists";
    } else {
       cout<<"file doesn't exist";
    }
-}   
+}
