cfc777433c308bd44ce13e8d6374f7f35cdd89f4
Maksymilian Kuś <max@MacBook-Air-Maksymilian.local>
2023-02-16 22:33:14 +0100

    Add krynica.cpp

--- krynica.cpp
+++ krynica.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main()
 {
 	ifstream file("input.txt");
-	
 	if(file.is_open())
 	{
 		char sign;
@@ -17,6 +16,8 @@ int main()
 	}
 	else
 		cout<<"This file doesnt exist.";
-	
+	int filesize = sizeof(FILE);
+   cout << "Size of file is "<< filesize;
+   int filesize2 = sizeof(FILE*);
 	return 0;
 }
