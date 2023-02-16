bc00009b51dd9cddaff84382467b022c12582e76
Maksymilian Kuś <max@MacBook-Air-Maksymilian.local>
2023-02-16 22:35:02 +0100

    Add krynica.cpp

--- krynica.cpp
+++ krynica.cpp
@@ -16,8 +16,10 @@ int main()
 	}
 	else
 		cout<<"This file doesnt exist.";
+
 	int filesize = sizeof(FILE);
    cout << "Size of file is "<< filesize;
    int filesize2 = sizeof(FILE*);
+   cout << "Size of *file is "<< filesize2;
 	return 0;
 }
